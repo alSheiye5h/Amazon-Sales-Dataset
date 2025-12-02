658cd4864ad931c95a4dd92950c68dbe550588c8
alSheiye5h <anasbelkhadir13@gmail.com>
2025-12-02 01:57:40 +0100

    _

--- scraper.py
+++ scraper.py
@@ -17,36 +17,23 @@ def scrap_page(url, scraper):
         price = float(product.find(class_="price_color").text[2:])
         in_stock = 0
         if [1 for r in product.find(class_="instock availability").i.get('class') if r == 'icon-ok'][0] == 1 : in_stock = 1
+        type_ = get_book_type(product, scraper)
         data = {
             "id": idx,
             "image": image,
             "stars": stars, 
             "title": title, 
+            "type": type_,
             "price": price, 
             "in_stock": in_stock,
         }
         products_list.append(data)
     return products_list
 
-scraper = cloudscraper.create_scraper(
-    browser={
-        "browser": "chrome",
-        "platform": "windows",
-    },
-)
-
 def get_book_type(product, scraper):
     href = get_href(product)
     url = f"https://books.toscrape.com/{href}"
     response = scraper.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    return soup
-
-p = {'id': '1000',
-  'image': '2cdad67c44b002e7ead0cc35693c0e8b.jpg',
-  'stars': 3,
-  'title': 'a light in the attic',
-  'price': 51.77,
-  'in_stock': 1}
-
-l = get_book_type(p, scraper)
+    type = soup.find(class_="breadcrumb").contents[5].a.text
+    return type
